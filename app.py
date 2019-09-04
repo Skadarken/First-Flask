82b3ceb2465d7eea363ce53ebaefe1049e3c4bdb
Skadarken <harishb159@gmail.com>
2019-09-04 20:55:54 +0530

    Pre-Endpoints

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -21,3 +21,4 @@ def index():
 
 if __name__=="__main__":
     app.run(debug=True)
+    
